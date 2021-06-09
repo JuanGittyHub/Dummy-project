216f67999973e265855ae92c334b64e3693c1d08
Juan-Lee <klink.juan@gmail.com>
2021-06-09 18:48:14 +0200

    First commit

---
+++ src/main/java/za/ac/cput/dummyproject/Tester.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.dummyproject;
+
+/**
+ *
+ * @author Juan
+ */
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println("Hello this is a test code");
+    }
+    
+}
